b2d0137accf1fd99ebcd92ae9d396d5f42a5120a
vinxe08 <me.vince.garcia@gmail.com>
2022-05-25 17:53:57 +0700

    Fix deployment issue about _mongoClientPromise issue for typescript

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -22,11 +22,11 @@ if (!process.env.MONGODB_URI) {
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options)
-    global._mongoClientPromise = client.connect()
+  if ((!global as any)._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    (global as any)._mongoClientPromise = client.connect()
   }
-  clientPromise = global._mongoClientPromise
+  clientPromise = (global as any)._mongoClientPromise
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options)
