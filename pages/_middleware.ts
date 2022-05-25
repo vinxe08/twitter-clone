9d8e06ee291ad83cd7d9046d96ff1ea36191c104
vinxe08 <me.vince.garcia@gmail.com>
2022-05-25 18:33:29 +0700

    Fix deployment issue in middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,23 +1,23 @@
 import { NextFetchEvent, NextResponse } from "next/server"
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+export async function middleware(request: NextRequest | any, ev: NextFetchEvent) {
   // Token will exist if user is logged in
   const session = await getToken({
-    req,
+    req: request,
     secret: process.env.JWT_SECRET,
     secureCookie: process.env.NODE_ENV == "production",
   });
 
-  const { pathname } = req.nextUrl
+  const { pathname } = request.nextUrl
 
   if(pathname.includes("/api/auth") || session) {
     return NextResponse.next();
   }
 
   if(!session && pathname !== "/") {
-    const url = req.nextUrl.clone()
+    const url = request.nextUrl.clone()
     url.pathname = '/'
     return NextResponse.rewrite(url)
 
